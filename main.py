a5e22a447b09e10d0c7d3f4472ed511998699d29
Nestor <delarosa.nestor@uabc.edu.mx>
2023-05-16 17:01:53 -0700

    Esto es arte

---
+++ main.py
@@ -0,0 +1,22 @@
+import colorama
+
+from art import *
+
+from colorama import init, Fore
+
+init ()
+
+tprint("art")
+
+aprint("random")
+
+tprint("texto de ejemplo texto de ejemplo")
+
+print(Fore.GREEN + "El profe", end = " ")
+
+print(Fore.RED + "Se la rifa", end = " ")
+
+print(Fore.BLUE + "Lo queremos", end = " ")
+
+print(Fore.YELLOW + "Mucho", end = " ")
+
